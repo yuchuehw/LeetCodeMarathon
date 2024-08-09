7341816ea6d2ac5e2acfbcfd8c9b8cfc14141d01
Yu-Chueh Wang <137755504+yuchuehw@users.noreply.github.com>
2024-08-09 15:38:04 -0700

    Update 840.py

--- 840.py
+++ 840.py
@@ -1,16 +1,27 @@
 import numpy as np
-NUMS = set(range(1,10))
+
+NUMS = set(range(1, 10))
+
+
 class Solution:
-  def numMagicSquaresInside(self, grid: List[List[int]]) -> int:
-    npgrid = np.array(grid)
-    height,width = npgrid.shape
-    counter = 0
-    for i in range(height):
-      for j in range(width):
-        subgrid = npgrid[i:i+3,j:j+3]
-        if (subgrid.size == (3,3) and set(subgrid.flatten())==NUMS
-          and sum(subgrid[0])==sum(subgrid[1])==sum(subgrid[2])==sum(subgrid[::,0])==sum(subgrid[::,1])==sum(subgrid[::,1])==sum(subgrid.diagonal())==sum(subgrid[::-1,::-1].diagonal())
-        ):
-          counter += 1
-    return counter
-          
+    def numMagicSquaresInside(self, grid: List[List[int]]) -> int:
+        npgrid = np.array(grid)
+        height, width = npgrid.shape
+        counter = 0
+        for i in range(height):
+            for j in range(width):
+                subgrid = npgrid[i : i + 3, j : j + 3]
+                if (
+                    subgrid.shape == (3, 3)
+                    and set(subgrid.flatten()) == NUMS
+                    and sum(subgrid[0])
+                    == sum(subgrid[1])
+                    == sum(subgrid[2])
+                    == sum(subgrid[::, 0])
+                    == sum(subgrid[::, 1])
+                    == sum(subgrid[::, 1])
+                    == sum(subgrid.diagonal())
+                    == sum(subgrid[::-1, ::-1].diagonal())
+                ):
+                    counter += 1
+        return counter
