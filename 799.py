ca1c7e5651cf8d3b9261e035670c9f92fa9b3602
Yu-Chueh Wang <137755504+yuchuehw@users.noreply.github.com>
2024-08-09 17:09:26 -0700

    Create 799.py

---
+++ 799.py
@@ -0,0 +1,12 @@
+class Solution:
+    def champagneTower(self, poured: int, query_row: int, query_glass: int) -> float:
+        tower = [[0]*i for i in range(1,101)]
+        tower[0][0] = poured
+        for i, row in enumerate(tower[:-1]):
+            for j in range(len(row)):
+                if row[j] > 1:
+                    over = row[j]-1
+                    tower[i+1][j] += over/2
+                    tower[i+1][j+1] += over/2
+                    row[j] = 1
+        return tower[query_row][query_glass]
